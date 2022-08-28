c903a4baedb714b93adce7d916b41a43083aabcd
yam-yam-yam-yam <77680103+yam-yam-yam-yam@users.noreply.github.com>
2022-08-28 14:43:18 +0900

    2022-08-28 | alert

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -62,26 +62,6 @@
     <v-card class="pa-4 ma-4 elevation-24">
         <v-alert
             class="my-2"
-            prominent
-            icon="mdi-firework"
-            max-width="500"
-            max-height="500"
-            position="relative"
-            color="red-darken-4"
-        >
-            Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Vivamus quis mi. Quisque ut nisi. Maecenas malesuada.
-        </v-alert>
-        <p>prominent (큰 아이콘)</p>
-        <p>icon="mdi-firework"</p>
-        <p>max-width="500"</p>
-        <p>max-height="500"</p>
-        <p>position="relative"</p>
-        <p>color="red-darken-4"</p>
-        <p>class="bg-teal-darken-4 text-red-darken-4" (이렇게도 사용 가능)</p>
-    </v-card>
-    <v-card class="pa-4 ma-4 elevation-24">
-        <v-alert
-            class="my-2"
             variant="elevated"
             tag="span"
         >
