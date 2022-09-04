062acb7b5cb8e082027f360428134c6c4869798c
yam-yam-yam-yam <77680103+yam-yam-yam-yam@users.noreply.github.com>
2022-09-04 15:00:51 +0900

    2022-09-04 | badge

---
+++ src/components/Badge.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-card class="pa-4 ma-4 elevation-24 bg-blue-grey-lighten-3">
+        <v-badge
+            class="my-2"
+            color="pink"
+            content="dasda"
+            text-color="blue-grey-lighten-3"
+            offset-x="30"
+            offset-y="-10"
+            bordered>
+            <v-btn
+                class="white--text"
+                color="pink-lighten-3"
+                depressed
+            >
+            Lock Account
+            </v-btn>
+        </v-badge>
+    </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
